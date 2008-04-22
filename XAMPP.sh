8fec64cb1ca7bb23fe2f1577ee10254cd687ac4e
FND <fnd@fnd-osmosoft.(none)>
2008-04-22 11:47:06 +0100

    added command-line switches

--- XAMPP.sh
+++ XAMPP.sh
@@ -3,13 +3,33 @@
 
 DIR="/tmp"
 
+function XAMPP() {
+	if [ $1 = true ]; then
+		echo "starting XAMPP"
+		sudo /opt/lampp/lampp start
+	elif [ $1 = false ]; then
+		echo "stopping XAMPP"
+		sudo /opt/lampp/lampp stop
+	fi
+}
+
+# command-line parameter
+if [ $1 = "e" ]; then
+	XAMPP true
+	exit
+elif [ $1 = "d" ]; then
+	XAMPP false
+	exit
+fi
+
+# selection menu
 PS3="XAMPP - Select action: "
 OPTIONS="start stop"
 select i in $OPTIONS; do 
 	if [ "$i" = "start" ]; then
-		sudo /opt/lampp/lampp start
+		XAMPP true
 	elif [ "$i" = "stop" ]; then
-		sudo /opt/lampp/lampp stop
+		XAMPP false
 	fi
 	exit
 done
