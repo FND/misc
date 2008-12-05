e11ca769d7e991174463e786aa0d83912f25314c
FND <fnd@fnd-osmosoft.(none)>
2008-12-05 09:36:20 +0000

    Twitter: removed hardcoded username

--- Twitter.sh
+++ Twitter.sh
@@ -1,13 +1,13 @@
 # To Do
-# * concatenate parameters $2-$n (i.e. no need for quotes)
+# * concatenate parameters $3-$n (i.e. no need for quotes)
 # * prompt for confirmation when >140 chars
 # * handle special chars (e.g. quotes, plus etc.)
 
-if [ ${#2} -le 0 ]; then
+if [ ${#3} -le 0 ]; then
 	echo "Error: missing password"	
-elif [ ${#2} -gt 140 ]; then
+elif [ ${#3} -gt 140 ]; then
 	echo "Error: message too long (${#2} characters)"
 else
-	echo "Posting: $2 (${#2} characters)"
-	curl -u FND:$1 -d status="$2" http://twitter.com/statuses/update.xml
+	echo "Posting: $3 (${#3} characters)"
+	curl -u $1:$2 -d status="$3" http://twitter.com/statuses/update.xml
 fi
