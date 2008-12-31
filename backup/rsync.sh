9c3b224d7fc000c8b1cace91a67030e5529458aa
FND <fnd@fnd-osmosoft.(none)>
2008-12-31 09:13:34 +0000

    backup: rotate directories

--- backup/rsync.sh
+++ backup/rsync.sh
@@ -9,8 +9,15 @@ fi
 # log start time
 STARTTIME=$(date +"%Y-%m-%d %H:%M:%S")
 
+# rotate backup directories
+BACKUPDIR="/mnt/backup/backup/fnd"
+mv $BACKUPDIR/current $BACKUPDIR/tmp
+mv $BACKUPDIR/alt $BACKUPDIR/current
+mv $BACKUPDIR/tmp $BACKUPDIR/alt
+
+# start sync'ing
 cd ~/Scripts/backup/
-rsync -a -e -v --progress ~ /mnt/backup/fnd/rsync/ --exclude-from="exclude.lst"
+rsync -a -e -v --progress ~ $BACKUPDIR/current/ --exclude-from="exclude.lst"
 
 # output duration
 echo "started: $STARTTIME"
