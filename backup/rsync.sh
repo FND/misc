1af9b20c9de683bc7a1a5e1ba832dd3b493782ed
FND <fnd@fnd-osmosoft.(none)>
2009-01-13 13:40:28 +0000

    backup: added support for minimal backups

--- backup/rsync.sh
+++ backup/rsync.sh
@@ -1,3 +1,22 @@
+# Usage:
+#  rsync.sh <full|minimal>
+#
+# To Do:
+# * sort out directory structure
+# * optimizations
+
+# check mode
+if [ "$1" = "full" ]; then
+	MODE=$1
+	BACKUPDIR="/mnt/backup/backup/fnd_minimal"
+elif [ "$1" = "minimal" ];
+	MODE=$1
+	BACKUPDIR="/mnt/backup/backup/fnd"
+else
+	echo "ERROR: no mode specified"
+	exit
+fi
+
 # check backup drive
 if mount | grep 'on /mnt/backup' >/dev/null; then
 	echo "starting backup process"
@@ -9,8 +28,7 @@ fi
 # log start time
 STARTTIME=$(date +"%Y-%m-%d %H:%M:%S")
 
-# rotate backup directories
-BACKUPDIR="/mnt/backup/backup/fnd"
+# rotate backup directories -- XXX: potentially unsafe/confusing
 mv $BACKUPDIR/current $BACKUPDIR/tmp
 mv $BACKUPDIR/alt $BACKUPDIR/current
 mv $BACKUPDIR/tmp $BACKUPDIR/alt
@@ -18,7 +36,11 @@ mv $BACKUPDIR/tmp $BACKUPDIR/alt
 # start sync'ing
 SOURCEDIR="~"
 cd ~/Scripts/backup/
-rsync -a -e -v --progress $SOURCEDIR $BACKUPDIR/current/ --exclude-from="exclude.lst"
+if [ "$1" = "full" ]; then
+	rsync -a -e -v --progress $SOURCEDIR $BACKUPDIR/current/ --exclude-from="exclude.lst"
+else
+	rsync -a -e -v --progress $SOURCEDIR $BACKUPDIR/current/ --include-from="include.lst"
+fi
 
 # log end time and duration
 ENDTIME=$(date +"%Y-%m-%d %H:%M:%S")
