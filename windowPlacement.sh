ec734f744b17bf523a1c5ac8a372d1d16e5ab7d5
FND <fnd@fnd-osmosoft.(none)>
2009-07-16 07:46:07 +0100

    window placement: adjusted coordinates for new screen arrangement

--- windowPlacement.sh
+++ windowPlacement.sh
@@ -1,3 +1,3 @@
-wmctrl -r XChat -e "0,1900,0,-1,-1"
-wmctrl -r Buddy -e "0,3076,0,-1,-1"
-wmctrl -r Gwibber -e "0,2935,0,-1,-1"
+wmctrl -r XChat -e "0,0,1200,-1,-1"
+wmctrl -r Buddy -e "0,1796,1200,-1,-1"
+wmctrl -r Gwibber -e "0,1755,1200,-1,-1"
