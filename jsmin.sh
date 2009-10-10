e96b37c3f2769a8a45b32983cd225b891c0969a6
FND <fnd@oda.(none)>
2009-10-10 12:00:40 +0100

    jsmin: various enhancements
    * calculating default filename
    * checking for YUICompressor
    * reporting errors
    * using lowercase variable names

--- jsmin.sh
+++ jsmin.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-SOURCE=$1
+# Usage:
+#   ./jsmin.sh source [target]
+
+srcfile=$1
 shift
-TARGET=$1 # TODO: $SOURCE.min.js
+outfile=$1
 
-# TODO: error messages
-[ -z $SOURCE ] && exit 1
-[ -z $TARGET ] && exit 1
-[ -f $TARGET ] && exit 1
+function quit {
+	echo "aborting: $1"
+	exit 1
+}
 
-STARTPATTERN="\/\*"
-ENDPATTERN="\*\/"
-HEADER=`cat $SOURCE \
-	| sed -e "/$ENDPATTERN/q" \
-	| sed -n "/^$STARTPATTERN/,/$ENDPATTERN/ p"`
+[ -z $srcfile ] && quit "source file not specified"
+if [ -z $outfile ]; then
+	outfile="${srcfile:0:${#srcfile}-3}.min.js"
+fi
+[ -f $outfile ] && quit "target file already exists"
+[ -f yuicompressor*.jar ] || quit "YUICompressor not found"
 
-echo "$HEADER" > $TARGET # TODO: should be optional
-java -jar yuicompressor-*.jar $SOURCE >> $TARGET
+startpattern="\/\*"
+endpattern="\*\/"
+header=`cat $srcfile \
+	| sed -e "/$endpattern/q" \
+	| sed -n "/^$startpattern/,/$endpattern/ p"`
+
+echo "$header" > $outfile # TODO: should be optional
+java -jar yuicompressor-*.jar $srcfile >> $outfile
