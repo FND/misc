ca0d545aac860656252097932a7022acdff2dc17
FND <fnd@oda.(none)>
2010-01-26 20:45:10 +0000

    added virtualenv wrapper

---
+++ virtualenv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+package=${1:?}
+directory=${2:-xvirt}
+
+echo "installing $package in $directory"
+
+# automatically adjust relative path for tarballs in CWD
+token=`echo $package | sed -e "s/^[^./].*\.tar\.gz$//"`
+if [ -z "$token" ]; then
+	package="../${package}"
+fi
+
+virtualenv --no-site-packages $directory && \
+cd $directory && \
+source bin/activate && \
+pip -E . install -U $package
