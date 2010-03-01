2ca1e04be80a58ccb67ac99de5ff35d06ccbb9ec
FND <fnd@oda.(none)>
2010-03-01 09:26:36 +0000

    randict: added support for passing in term as argument
    That makes the script a bit of a misnomer...

--- randict.sh
+++ randict.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
-term=$(~/Scripts/todotxt/todo.sh ls @vocab | shuf -n1 | \
-	sed -e "s/@[^ ][^ ]*//g" -e "s/[0-9 ]*\(.*\) */\1/")
+term="$@"
+if [ -z "$term" ]; then
+	term=$(~/Scripts/todotxt/todo.sh ls @vocab | shuf -n1 | \
+		sed -e "s/@[^ ][^ ]*//g" -e "s/[0-9 ]*\(.*\) */\1/")
+fi
+
 echo "looking up $term"
 w3m -no-cookie -dump "http://definr.com/definr/show/$term"
