886661e0cf35a7a7cd8487dd2ea83b562a8e579a
FND <fnd@oda.(none)>
2010-05-03 07:54:46 +0100

    added HTML validation script
    using the W3C's web API
    not parsing the results yet

---
+++ htmlValidator.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+filename=${1:?}
+
+tmpfile="/tmp/validtor$$.html"
+
+echo "validating $filename..."
+curl -X POST -F uploaded_file=@"$filename" -F charset="(detect automatically)" \
+	-F doctype=inline -F group=0 -s http://validator.w3.org/check > $tmpfile # -F output=json
+w3m $tmpfile
+rm $tmpfile
