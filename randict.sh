627275f29bebe0bec5de0588ab11b27419c3c7f9
FND <fnd@fnd-oda.(none)>
2010-08-24 17:31:46 +0200

    fixed typo, quoting

--- randict.sh
+++ randict.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 term="$@"
-if [ -z $term ]; then
+if [ -z "$term" ]; then
 	term=$(~/Scripts/todotxt/todo.sh ls @vocab | shuf -n1 | \
 		sed -e "s/@[^ ][^ ]*//g" -e "s/[0-9 ]*\(.*\) */\1/")
 fi
