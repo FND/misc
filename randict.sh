b2f668f5376cf4ad33bc2c3330fd59f3b6bbfadc
FND <fnd@fnd-oda.(none)>
2010-11-05 07:47:19 +0100

    randict: refactored for improved reporting and flexibility
    definr.com was down for a few days, prompting the use of alternatives

--- randict.sh
+++ randict.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
+uri="http://definr.com/definr/show/"
+#uri="http://wordnetweb.princeton.edu/perl/webwn?sub=Search+WordNet&o0=1&o1=1&s="
+#uri="http://www.google.com/search?q=define%3A"
+
 term="$@"
 if [ -z "$term" ]; then
 	term=$(~/Scripts/todotxt/todo.sh ls @vocab | shuf -n1 | \
 		sed -e "s/@[^ ][^ ]*//g" -e "s/[0-9 ]*\(.*\) */\1/")
 fi
 
+uri="$uri$term" # XXX: assumes term is always at the end
+
 echo "looking up $term"
-w3m -no-cookie -dump "http://definr.com/definr/show/$term"
+echo "$uri"
+w3m -no-cookie -dump "$uri" #| less
